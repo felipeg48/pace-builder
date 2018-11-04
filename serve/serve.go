e94194380298b3ab9818200248de4b809b1f2e9d
Peter Blum <pblum@pivotal.io>
2018-11-04 09:17:05 -0500

    quick fix to return errors correctly

--- serve/serve.go
+++ serve/serve.go
@@ -15,7 +15,7 @@ func ServeCmd() {
 
 	fmt.Println("Checking workshopGen")
 	if err := os.Chdir("workshopGen/"); err != nil {
-		fmt.Println("Please use build before serve to create the content. Error:" + err.Error())
+		fmt.Println("Please `build` before `serve` to create the content. Error:" + err.Error())
 		return
 	}
 
@@ -29,7 +29,7 @@ func ServeCmd() {
 func checkDependencies() error {
 	cmd := exec.Command("/bin/sh", "-c", "command -v "+"hugo")
 	if err := cmd.Run(); err != nil {
-		fmt.Println("It looks like Hugo is not installed! You need to install Hugo to run a local instance of the web site...")
+		return fmt.Errorf("It looks like Hugo is not installed! You need to install Hugo to run a local instance of the web site...")
 	}
 	return nil
 }
@@ -37,7 +37,7 @@ func checkDependencies() error {
 func serveHugo() error {
 	cmd := exec.Command("hugo", "serve")
 	if err := cmd.Run(); err != nil {
-		fmt.Println("Error running Hugo")
+		return fmt.Errorf("Error running Hugo: %s", err)
 	}
 	return nil
 }
